34f81906840cc99ed8c8514bb5d5c5a3fc417d77
Roman <parovozdmb@mail.ru>
2024-07-08 17:09:41 +0400

    naming

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int doPOpolneniya = 5000;
-        int summaPopolneniya = 9000;
-        int kolichestvoBonusov = summaPopolneniya / 100;
+        int underReplenishment = 5000;
+        int amountReplenishment = 9000;
+        int bonus = amountReplenishment / 100;
 
         int resault;
-        if (summaPopolneniya < 1000) {
-            resault = doPOpolneniya + summaPopolneniya;
+        if (amountReplenishment < 1000) {
+            resault = underReplenishment + amountReplenishment;
         } else {
-            resault = doPOpolneniya + summaPopolneniya + kolichestvoBonusov;
+            resault = underReplenishment + amountReplenishment + bonus;
         }
         System.out.println("Сумма на счете: " + resault + " рублей ");
 
